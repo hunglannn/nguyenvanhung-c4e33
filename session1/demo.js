63a1cfd29675e62ae3747ee080d44759b94819e9
hunglannn <hungrann.nguyen@gmail.com>
2019-05-14 22:06:13 +0700

    sdf

---
+++ session1/demo.js
@@ -0,0 +1,22 @@
+// let name = "1";
+// name = "5";
+// console.log("hello",name);
+
+// 
+// let x = Number (prompt("Nhập số"));
+// let y = Number (prompt("Nhập số"));
+console.log(x+y);
+
+clear()
+
+for (let i = 0; i < 5; i++) {
+    fd(100)
+    rt(90)
+    fd(100)
+    rt(90)
+    fd(100)
+    rt(90)
+    fd(100)
+    rt(90) 
+    rt(3)  
+}
