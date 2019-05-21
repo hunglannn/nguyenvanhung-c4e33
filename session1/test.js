4b4498004cb0bb99c1d63999bb07ea2158bb4f4b
hunglannn <hungran.nguyen@gmail.com>
2019-05-21 15:36:30 +0700

    hw

---
+++ session1/test.js
@@ -0,0 +1,28 @@
+let x = Number (prompt( "Radius?") );
+console.log (x * x * 3.14);
+let y = Number (prompt("(C)") );
+console.log  ((9/5 * y)+32 )
+
+// Hình 1
+clear()
+rt(90)
+fd(100)
+lt(90)
+fd(100)
+lt(90)
+fd(100)
+lt(90)
+fd(100)
+lt(90)
+
+// Hình 2
+clear()
+rt(90)
+fd(100)
+lt(120)
+fd(100)
+lt(120)
+fd(100)
+lt(120)
+
+// câu 1: use typeof to check javascript data
