ba1f96796e0a72da8af531cc17ba6094a32f6978
hunglannn <hungran.nguyen@gmail.com>
2019-06-04 17:49:54 +0700

    asdjasdsa

--- session5/hw.js
+++ session5/hw.js
@@ -69,8 +69,12 @@
 // ex2.6
 
 let remove_dollar_sign = (s) =>{
-    let remove = s.replace("$", " ")
+    let remove = s.replace(/[$]/g, "")
     return remove
 }
-let s = prompt("Enter $")
-console.log(remove_dollar_sign)
+console.log(remove_dollar_sign("$$$$$$hello"))
+string_with_no_dollars = remove_dollar_sign("$80% percent of $life is to show $up")
+if (string_with_no_dollars == "80% percent of life is to show up"){
+    console.log("Your function is correct")}
+else{
+    console.log("Oops, there's a bug")}
