2925d7cd88455ad18599be221fa6c0219ce22afe
hunglannn <hungran.nguyen@gmail.com>
2019-07-07 23:48:44 +0700

    hshop

---
+++ hshop/hshop.js
@@ -0,0 +1,10 @@
+// function myfunction () {
+//         let x = document.getElementById('tablediv');
+//         if (x.style.display === 'none') {
+//             x.style.display = 'block';
+//         } 
+//         else {
+//             x.style.display = 'none';
+//         }
+   
+// }
